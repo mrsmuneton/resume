19abb76f07969e61820c3ea43e1e6c7c5f34cb05
Ray <rmuneton@creativechannel.com>
2016-04-08 11:01:03 -0700

    fri morning

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'resume',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://rays-resume.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
