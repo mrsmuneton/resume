40134fd689c1cf652668e99222f955aa3097119a
Ray Muneton <ray.muneton@hallmarklabs.com>
2018-01-21 20:22:44 -0800

    Styling

--- app/components/skill-display.js
+++ app/components/skill-display.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
       var name = document.getElementsByClassName("skill-name");
       for (var i = 0; i < name.length; i++) {
         name[i].style.textDecoration = 'none';
+        name[i].style.fontWeight = 'normal'
       }
       var details = document.getElementsByClassName("skill-detail");
       for (var i = 0; i < details.length; i++) {
@@ -13,6 +14,7 @@ export default Ember.Component.extend({
       }
   		document.getElementById(someskill.id.toString() + '-d' ).style.fontWeight = '900';
       document.getElementById(someskill.id.toString() + '-n' ).style.textDecoration = 'underline';
+      document.getElementById(someskill.id.toString() + '-n' ).style.fontWeight = '800';
     }
   }
 });
